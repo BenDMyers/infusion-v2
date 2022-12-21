778c2027bc860ed2c04ae5a959b0ad2af80de434
Ben Myers <ben.myers@okstate.edu>
2022-12-21 11:27:21 -0600

    Ensure slugs are smarter about slashes and ampersands

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -1,6 +1,8 @@
 import removeAccents from 'remove-accents';
 
 const SPACES = / /g;
+const SLASHES = /\//g;
+const AMPERSANDS = /&/g;
 const NONALPHANUMERIC = /[^A-Za-z0-9-]/g;
 const MULTIPLE_HYPHENS = /--+/g;
 
@@ -13,6 +15,8 @@ export function sluggify(name: string) {
 		.trim()
 		.toLowerCase()
 		.replace(SPACES, '-')
+		.replace(SLASHES, '-')
+		.replace(AMPERSANDS, '-and-')
 		.replace(NONALPHANUMERIC, '')
 		.replace(MULTIPLE_HYPHENS, '-');
 
