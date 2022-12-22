31778536d2fa6837d22061841898bf22dcf6dc08
Ben Myers <ben.myers@okstate.edu>
2022-12-21 22:04:41 -0600

    Implement Bea's feedback

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -3,6 +3,7 @@ import removeAccents from 'remove-accents';
 const SPACES = / /g;
 const SLASHES = /\//g;
 const AMPERSANDS = /&/g;
+const PLUSSES = /\+/g;
 const NONALPHANUMERIC = /[^A-Za-z0-9-]/g;
 const MULTIPLE_HYPHENS = /--+/g;
 
@@ -17,6 +18,7 @@ export function sluggify(name: string) {
 		.replace(SPACES, '-')
 		.replace(SLASHES, '-')
 		.replace(AMPERSANDS, '-and-')
+		.replace(PLUSSES, '-and-')
 		.replace(NONALPHANUMERIC, '')
 		.replace(MULTIPLE_HYPHENS, '-');
 
