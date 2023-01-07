236e3a7d5aa9d6d0c445c1b599bd6e725b9e05b8
Ben Myers <ben.myers@okstate.edu>
2023-01-07 08:56:40 -0600

    Mini cleanups

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -6,6 +6,7 @@ const AMPERSANDS = /&/g;
 const PLUSSES = /\+/g;
 const NONALPHANUMERIC = /[^A-Za-z0-9-]/g;
 const MULTIPLE_HYPHENS = /--+/g;
+const TRAILING_HYPHENS = /-+$/;
 
 export function sluggify(name: string) {
 	if (!name) {
@@ -20,7 +21,8 @@ export function sluggify(name: string) {
 		.replace(AMPERSANDS, '-and-')
 		.replace(PLUSSES, '-and-')
 		.replace(NONALPHANUMERIC, '')
-		.replace(MULTIPLE_HYPHENS, '-');
+		.replace(MULTIPLE_HYPHENS, '-')
+		.replace(TRAILING_HYPHENS, '');
 
 	return slug;
 }
